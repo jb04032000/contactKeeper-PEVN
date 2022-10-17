db67f1780b523193ade41455ff83a7ae85d18b47
jb0403 <jayeshb7548@gmail.com>
2022-10-17 11:35:44 +0530

    -added heroku configuration

--- client/src/utils/apiUrls.js
+++ client/src/utils/apiUrls.js
@@ -1,4 +1,5 @@
-export const baseUrl = "http://localhost:8080/";
+// export const baseUrl = "http://localhost:8080/";
+export const baseUrl = "https://contactkeeper-pevn-by-jb.herokuapp.com/";
 
 const API_URLS = {
   auth: "api/auth",
