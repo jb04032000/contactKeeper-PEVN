7e20983bf9fd3284c675f873c341636dcffed814
jb0403 <jayeshb7548@gmail.com>
2022-10-17 15:10:57 +0530

    -

--- database/config/config.js
+++ database/config/config.js
@@ -4,13 +4,42 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
+
+// const pool = new Pool({
+//   user: process.env.POSTGRES_USER,
+//   host: process.env.POSTGRES_HOST,
+//   database: process.env.POSTGRES_DB,
+//   password: process.env.POSTGRES_PASSWORD,
+//   port: process.env.POSTGRES_PORT,
+//   ssl: {
+//       rejectUnauthorized: false,
+//   }
+// });
